a306578cf8f8c444bf9380c13e5784bc78fed386
AxelMth <axel.mathieu@telecom-paristech.fr>
2016-12-13 21:42:05 +0100

    Rendu des exercices 2

--- exercice-cross-compilation/test.c
+++ exercice-cross-compilation/test.c
@@ -10,13 +10,11 @@ int main(int argc, char** argv) {
         int localvar;
         int localvar2;
         int* heap = NULL;
-        pid_t pid = getpid();
 
         printf("BSS is at %.16llx\n", &bss);
 
         heap = (int*) malloc(sizeof(int));
         printf("heap is at %.16llx\n", heap);
-
         printf("stack is at %.16llx\n", &localvar);
         printf("stack2 is at %.16llx\n", &localvar2);
 
